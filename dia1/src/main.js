e43345fc6dda54842ec20684dcb98d4a20ee6c0c
igorcesarcode <igorcesarbernades@gmail.com>
2021-08-17 16:39:43 -0300

    exercio 4

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -1,4 +1,13 @@
 import '../src/style.css'
-
-document.querySelector('[data-js="app"]').innerHTML = ` <h1>B. Academy</h1> <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
+const link = document.querySelector('[data-js="link"]')
+const app = document.querySelector('[data-js="app"]')
+const dados = ` <h1>B. Academy</h1> <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `;
+
+app.innerHTML = dados;
+
+link.addEventListener('click', (e)=>{
+  e.preventDefault();
+  //Por padrão o style.visibility vem vazio.
+  app.style.visibility = (app.style.visibility === '' ? "hidden" : "");
+});

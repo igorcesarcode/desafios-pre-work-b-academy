5e228bcff308113e74bde71defa2f6a05ce6d39a
igorcesarcode <igorcesarbernades@gmail.com>
2021-08-20 14:57:26 -0300

    resolução do exercicio 2

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,21 +1,30 @@
 //Exercício 1
 
 const textoDigitado = document.querySelector('[data-js="textname"]')
-
+const naoDas =['Da', 'Das', 'De', 'Do', 'Dos']
 String.prototype.normalizar = function() {
   //Deixa Todo Mundo minusculo
   let temp = this.toLowerCase();
   //Deixa as Primeiras letras em maiusculo.
-  return temp.replace(/^(.)|\s+(.)/g, c => c.toUpperCase())
+  temp = temp.replace(/^(.)|\s+(.)/g, c => c.toUpperCase())
   //Deixas os "de", "da", "do" ou "dos" em minusculos
   //Não Conseguir achar o regex pra isso.
-}
+  return temp
 
+}
 textoDigitado.addEventListener('input', (e)=>{
   e.target.value = e.target.value.normalizar()
-  console.log(temp)
-
 })
 
 
 //Exercício 2
+const corSelecionada = document.querySelector('[data-js="coresSelecionada"]')
+const app = document.querySelector('[data-js="cordiv"]')
+console.log(corSelecionada.selectedOptions)
+
+corSelecionada.addEventListener('change', (e) => {
+  let dados = [];
+  [...e.target.selectedOptions].map(el =>
+  dados = [...dados ,(`<div class="quadrado" style="background-color: ${el.value};">${el.value} </div>`)])
+  app.innerHTML = dados.toString().replace(/,/g, "")
+})
